72a5193014b7f70b02b89d3367b093d8de987828
Theodore Colbert <hubedybubedy123@gmail.com>
2025-09-12 16:13:41 +0100

    Stabilise input by averaging

--- Assets/Scripts/Serial/EnginesSerial.cs
+++ Assets/Scripts/Serial/EnginesSerial.cs
@@ -1,64 +1,101 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class EnginesSerial : MonoBehaviour
 {
-  [SerializeField] private float depthVal;
-  [SerializeField] private float steerVal;
-  [SerializeField] private float thrustVal;
+  [SerializeField] private int depthVal;
+  [SerializeField] private int steerVal;
+  [SerializeField] private int thrustVal;
 
   [SerializeField] private SerialControllerCustomDelimiter serialController;
   [SerializeField] private bool connected = false;
 
-  private const byte DEPTH =      0b01000000;
+  private const byte DEPTH = 0b01000000;
   private const byte BEARING =    0b10000000;
   private const byte SPEED =      0b11000000;
 
-  public enum DisplayType {
+  private const int INPUT_SAMPLES = 10;
+
+  private int[] depthSamples;
+  private int[] steerSamples;
+  private int[] thrustSamples;
+
+  private int sample = 0;
+
+  public enum DisplayType
+  {
     DEPTH,
     BEARING,
     SPEED
   }
 
-  void Awake() {
-    depthVal = 50f;
-    steerVal = 50f;
-    thrustVal = 50f;
+  void Awake()
+  {
+    depthVal = 50;
+    steerVal = 50;
+    thrustVal = 50;
+
+    sample = 0;
+    depthSamples = new int[INPUT_SAMPLES];
+    steerSamples = new int[INPUT_SAMPLES];
+    thrustSamples = new int[INPUT_SAMPLES];
   }
 
-  public float GetDepthVal() {
+  public float GetDepthVal()
+  {
     return (50f-depthVal) / 50f;
   }
 
-  public float GetSteerVal() {
+  public float GetSteerVal()
+  {
     return (50f-steerVal) / 50f;
   }
 
-  public float GetThrustVal() {
+  public float GetThrustVal()
+  {
     return (thrustVal-50f) / 50f;
   }
 
   // Read joystick values between 0-100
-  void OnMessageArrived(byte[] message) {
+  void OnMessageArrived(byte[] message)
+  {
     if (message.Length != 3) return;
-    if (!connected) {
+    if (!connected)
+    {
       Debug.Log("Engines connected");
       connected = true;
     }
 
-    depthVal = message[0];
-    steerVal = message[1];
-    thrustVal = message[2];
+    depthSamples[sample] = message[0];
+    steerSamples[sample] = message[1];
+    thrustSamples[sample] = message[2];
+
+    if (++sample >= INPUT_SAMPLES)
+    {
+      sample = 0;
+
+      depthVal = (int) Math.Round(depthSamples.Average(), 0);
+      steerVal = (int) Math.Round(steerSamples.Average(), 0);
+      thrustVal = (int) Math.Round(thrustSamples.Average(), 0);
+    }
+    
   }
 
-  public void UpdateDisplay(int value, DisplayType type) {
+  public void UpdateDisplay(int value, DisplayType type)
+  {
     byte[] valueBytes = BitConverter.GetBytes(value);
 
-    if (type.Equals(DisplayType.DEPTH)) {
+    if (type.Equals(DisplayType.DEPTH))
+    {
       valueBytes[1] = (byte)(valueBytes[1] | DEPTH);
-    } else if (type.Equals(DisplayType.BEARING)) {
+    }
+    else if (type.Equals(DisplayType.BEARING))
+    {
       valueBytes[1] = (byte)(valueBytes[1] | BEARING);
-    } else if (type.Equals(DisplayType.SPEED)) {
+    }
+    else if (type.Equals(DisplayType.SPEED))
+    {
       valueBytes[1] = (byte)(valueBytes[1] | SPEED);
     }
 
